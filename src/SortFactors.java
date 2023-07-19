b47c69e3f3ee331c1bb2f7b5767a7caccdce9603
Thanusuyasri <82963993+Thanusuyasri@users.noreply.github.com>
2023-07-19 18:34:22 +0530

    first commit

--- src/SortFactors.java
+++ src/SortFactors.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class SortFactors {
+import java.util.Scanner;
+
+public class SortFactors {
+    public static int countOfFactors(int n) {
+        int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (i == n / i) {
+                    count++;
+                } else {
+                    count += 2;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] a = new int[size];
+        for (int i = 0; i < size; i++) {
+            a[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if ((countOfFactors(a[i]) > countOfFactors(a[j])) || a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    for(int i=0;i<size;i++)
+    {
+        System.out.println(a[i]);
+    }
+
+    }
 }
